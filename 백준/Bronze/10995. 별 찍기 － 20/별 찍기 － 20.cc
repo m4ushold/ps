b0db48c3a3a9c060eefec097731fbcece095e97b
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-02-23 10:56:44 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+	int n; cin >> n;
+    for(int i=0;i<n;i++) {
+        if(i&1) cout << ' ';
+        for(int j=0;j<n;j++) cout << "* ";
+        cout << '\n';
+    }
+	return 0;
+}
