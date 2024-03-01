a399e5ac2e5622e3285c81d4b82347969f6b4e0c
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-01 18:14:18 +0900

    [Diamond V] Title: Smallest Enclosing Sphere, Time: 120 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Diamond/11930. Smallest Enclosing Sphere/Smallest Enclosing Sphere.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lf=double;
+using Point=tuple<lf,lf,lf>;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    vector<Point> v(n);
+    lf x,y,z,a=0.1,r=0;
+    for(auto &[a,b,c]:v) cin >> a >> b >> c, x+=a, y+=b, z+=c;
+    x/=n, y/=n, z/=n;
+    
+    for(int i=0,idx;i<27182;i++) {
+        idx=r=0;
+        for(int j=0;j<n;j++) {
+            auto [xx,yy,zz]=v[j];
+            lf dx=x-xx, dy=y-yy, dz=z-zz, d=sqrt(dx*dx+dy*dy+dz*dz);
+            if(d>r) r=d, idx=j;
+        }
+        auto [xx,yy,zz]=v[idx];
+        x+=(xx-x)*a, y+=(yy-y)*a, z+=(zz-z)*a;
+        a*=0.999;
+    }
+    if(abs(r) < 1e-7) r = 0;
+    cout << fixed << setprecision(2) << r;
+    return 0;
+}
