12d41f4c5f9e8f4bb6f33717630f32610f0ef7ef
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-02 21:43:08 +0900

    [Platinum V] Title: 문자열 제곱, Time: 92 ms, Memory: 7076 KB -BaekjoonHub

---
+++ 백준/Platinum/4354. 문자열 제곱/문자열 제곱.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> GetFail(const string &p){
+    int n = p.size();
+    vector<int> fail(n);
+    for(int i=1, j=0; i<n; i++){
+        while(j && p[i] != p[j]) j = fail[j-1];
+        if(p[i] == p[j]) fail[i] = ++j;
+    }
+    return fail;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    string s; 
+    while(cin >> s && s!=".") {
+        int n=s.size(), m=n-GetFail(s)[n-1];
+        if(n%m==0) cout << n/m << '\n';
+        else cout << "1\n";
+    }
+    return 0;
+}
