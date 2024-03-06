d2c809cf57b05e3d7f88e7f0bc97c2c5b122c91a
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-06 21:22:50 +0900

    [Platinum I] Title: 수열과 쿼리 6, Time: 216 ms, Memory: 6048 KB -BaekjoonHub

---
+++ 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int N, B, A[101010], R[101010], C[101010], D[101010], MX;
+struct Qry {
+    int idx, s, e;
+    bool operator<(Qry &a) { return s/B == a.s/B ? e < a.e : s/B < a.s/B; }
+};
+
+vector<Qry> qry;
+
+void sub(int v) {
+    if(C[v]==MX && D[C[v]]==1) MX--;
+    D[C[v]]--, D[C[v]-1]++, C[v]--;
+}
+
+void add(int v) {
+    if(C[v]==MX && !D[C[v]+1]) MX++;
+    D[C[v]+1]++, D[C[v]]--, C[v]++;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N; B=sqrt(N);
+    for(int i=1;i<=N;i++) cin >> A[i];
+    int q; cin >> q;
+    for(int i=0,s,e;i<q;i++) cin >> s >> e, qry.push_back({i,s,e});
+    sort(qry.begin(),qry.end());
+
+    D[0]=N;
+    int s=qry[0].s, e=qry[0].e;
+    for(int i=s;i<=e;i++) add(A[i]);
+	R[qry[0].idx]=MX;
+	for(int i=1; i<q; i++){
+		while(s < qry[i].s) sub(A[s++]);
+		while(s > qry[i].s) add(A[--s]);
+		while(e < qry[i].e) add(A[++e]);
+		while(e > qry[i].e) sub(A[e--]);
+		R[qry[i].idx] = MX;
+	}
+	for(int i=0; i<q; i++) cout << R[i] << "\n";
+    return 0;
+}
