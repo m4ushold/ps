9ea861cb93eb44ad4e018760600f2c8f1ba36eb8
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-06 17:40:54 +0900

    [Bronze IV] Title: Champernowne Verification, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31520. Champernowne Verification/Champernowne Verification.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    string s,t="123456789"; cin >> s;
+    cout << (s == t.substr(0,min((int)s.size(),9)) ? (int)s.size() : -1);
+    return 0;
+}
