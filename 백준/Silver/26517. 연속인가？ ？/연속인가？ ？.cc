08e81053f4ba520c33a9475b5e7ae5e1a81156e7
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-10 15:49:05 +0900

    [Silver V] Title: 연속인가? ?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll k,a,b,c,d; cin >> k >> a >> b >> c >> d;
+    if(a*k+b == c*k+d) cout << "Yes " << a*k+b << '\n';
+    else cout << "No\n";
+    return 0;
+}
