64356a982ba13e78b84352206c355dec67f11c32
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-25 18:50:58 +0900

    [Gold V] Title: KSA 문자열, Time: 8 ms, Memory: 2912 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char NXT[202];
-
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
-    NXT['K']='S', NXT['S']='A', NXT['A']='K';
-    string s; cin >> s;
-    int ans=1e9, tmp=0, n=s.size();
-    
-    char t='K';
-    for(char c:s) if(c==t) tmp++, t=NXT[t];
-    tmp=n-tmp;
-    ans=min(ans,tmp*2);
+    string s,ksa="KSA"; cin >> s;
+    int i=0, cnt=0, res=1e9;
+    for(char c:s) c==ksa[i%3] ? ++i : ++cnt;
+    cnt+=abs(i-(int)s.size());
+    res=min(res,cnt);
 
-    t='S', tmp=0;
-    for(char c:s) if(c==t) tmp++, t=NXT[t];
-    tmp=n-tmp;
-    ans=min(ans,tmp*2+2);
-    
-    t='A', tmp=0;
-    for(char c:s) if(c==t) tmp++, t=NXT[t];
-    tmp=n-tmp;
-    ans=min(ans,tmp*2+4);
+    i=1, cnt=1;
+    for(char c:s) c==ksa[i%3] ? ++i : ++cnt;
+    cnt+=abs(i-(int)s.size());
+    res=min(res,cnt);
 
-    cout << ans;
+    i=2, cnt=2;
+    for(char c:s) c==ksa[i%3] ? ++i : ++cnt;
+    cnt+=abs(i-(int)s.size());
+    res=min(res,cnt);
+
+    cout << res;
     return 0;
 }
