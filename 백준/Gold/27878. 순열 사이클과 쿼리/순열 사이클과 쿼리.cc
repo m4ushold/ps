e265654de7871b85ba67500fabb9ca80fb943b1b
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-03-26 18:44:22 +0900

    [Gold III] Title: 순열 사이클과 쿼리, Time: 5416 ms, Memory: 6328 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/27878. 순열 사이클과 쿼리/순열 사이클과 쿼리.cc
+++ 백준/Gold/27878. 순열 사이클과 쿼리/순열 사이클과 쿼리.cc
@@ -2,47 +2,42 @@
 using namespace std;
 using ll=long long;
 
-ll N, Q, R, A[101010], B[101010], C[101010];
-vector<int> V;
+ll N, Q, A[101010], B[101010], C[101010], MOD[101010];
 string M;
 
-int mod(int p) {
-    int res=0;
-    for (char c:M) res=(res*10+(c-'0'))%p;
+ll m(int p) {
+    ll res=0;
+    for(char c:M) res=(res*10+c-'0')%p;
     return res;
 }
 
-int dfs(int v, int d=0) {
-    if(C[v]) return d;
-    V.push_back(v);
-    C[v]=1;
-    return dfs(A[v],d+1);
-}
-
-void f() {
-    for(int i=1;i<=N;i++) B[i]=C[i]=0;
-
+ll calc() {
+    memset(B, 0, sizeof B), memset(C, 0, sizeof C);
     for(int i=1;i<=N;i++) if(!C[i]) {
-        int p=mod(dfs(i));
-        for(int j=0;j<V.size();j++) B[V[j]]=V[(j+p)%V.size()];
-        V.clear();
-    }
+        int v=i, p=0;
+        vector<int> vec;
+        while(!C[v]) C[v]=1, vec.push_back(v), v=A[v], p++;
 
-    R=0;
-    for(int i=1;i<=N;i++) R+=i*B[i];
+        for(int j=0;j<p;j++) B[vec[j]]=vec[(j+MOD[p])%p];
+    }
+    
+    ll res=0;
+    for(int i=1;i<=N;i++) res+=B[i]*i;
+    return res;
 }
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> N >> M;
-    for(int i=1;i<=N;i++) cin >> A[i], R+=i*A[i];
+    for(int i=1;i<=N;i++) cin >> A[i];
+    for(int i=1;i<101010;i++) MOD[i]=m(i);
 
     cin >> Q;
-    while(Q--) {
-        int a,b,c; cin >> a;
-        if(a&1) cin >> b >> c, swap(A[b], A[c]);
-        else f(), cout << R << '\n';
+    for(int q=0,a,i,j;q<Q;q++) {
+        cin >> a;
+        if(a&1) cin >> i >> j, swap(A[i], A[j]);
+        else cout << calc() << '\n';
     }
     return 0;
 }
