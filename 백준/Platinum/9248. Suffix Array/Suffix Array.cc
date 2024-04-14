5ef33c2b5c5dfa7ce1cdb56a32edf25b67796009
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-04-14 13:56:38 +0900

    [Platinum III] Title: Suffix Array, Time: 540 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<vector<int>,vector<int>> sfx(const string& s) {
+    int n=s.size();
+    vector<int> sa(n), pos(n), tmp(n);
+    for(int i=0;i<n;i++) sa[i]=i, pos[i]=s[i];
+    for(int k=1;;k*=2) {
+        auto cmp = [&](int a, int b) {
+            if(pos[a]!=pos[b]) return pos[a]<pos[b];
+            if(a+k<n && b+k<n) return pos[a+k]<pos[b+k];
+            return a>b;
+        };
+        sort(sa.begin(),sa.end(),cmp);
+        for(int i=1;i<n;i++) tmp[i]=tmp[i-1]+cmp(sa[i-1],sa[i]);
+        for(int i=0;i<n;i++) pos[sa[i]]=tmp[i];
+        if(tmp.back()+1==n) break;
+    }
+
+    vector<int> lcp(n);
+    for(int i=0,j=0;i<n;i++,j=max(j-1,0)) {
+        if(pos[i]==0) continue;
+        while(sa[pos[i]-1]+j < n && sa[pos[i]]+j < n && s[sa[pos[i]-1]+j] == s[sa[pos[i]]+j]) j++;
+        lcp[pos[i]]=j;
+    }
+    return {sa,lcp};
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    string s; cin >> s;
+    auto [sa, lcp]=sfx(s);
+    for(int i:sa) cout << i+1 << ' ';
+    cout << "\nx ";
+    for(int i=1;i<lcp.size();i++) cout << lcp[i] << " ";
+    return 0;
+}
