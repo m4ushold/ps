1e05472c2b44d3017ad9bc060489b8a0de857c06
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-04-17 15:42:20 +0900

    [Silver I] Title: 관중석, Time: 0 ms, Memory: 2024 KB, Score: 19 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.cc
+++ 백준/Silver/10166. 관중석/관중석.cc
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
     int a,b; cin >> a >> b;
-    int dap=0;
-    for(int i=a;i<=b;i++) {
-        int cnt=i-1;
-        for(int j=a;j<i;j++) cnt-=gcd(j,i)-1;
-        dap+=cnt;
+    ll dap=a;
+    for(int i=a+1;i<=b;i++) {
+        set<int> s;
+        s.insert(1);
+        for(int j=a,g;j<i;j++) {
+            g=gcd(i,j);
+            if(g!=1) for(int k=g;k<i;k+=g) s.insert(k);
+        }
+        dap+=i-s.size();
     }
-    cout << dap+1;
+    cout << dap;
     return 0;
 }
