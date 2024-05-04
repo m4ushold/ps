54b34eb7b81e80ab730d4e7ba192e14af7436bbe
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-05-04 10:45:25 +0900

    [Platinum I] Title: 큰 수 곱셈 (2), Time: 1200 ms, Memory: 117796 KB -BaekjoonHub

--- 백준/Platinum/15576. 큰 수 곱셈 （2）/큰 수 곱셈 （2）.cc
+++ 백준/Platinum/15576. 큰 수 곱셈 （2）/큰 수 곱셈 （2）.cc
@@ -4,28 +4,25 @@ using ll=long long;
 using cpx=complex<double>;
 using poly=vector<cpx>;
 
-poly dft(const poly &f, cpx w) {
+void dft(poly &f, cpx w) {
     int n=f.size();
-    if(n==1) return f;
+    if(n==1) return;
 
     poly a,b,c(n);
     for(int i=0;i<n;i++) i&1 ? a.push_back(f[i]) : b.push_back(f[i]);
-    a=dft(a, w*w), b=dft(b,w*w);
+    dft(a, w*w), dft(b,w*w);
 
     cpx wp(1,0);
     for(int i=0;i<n/2;i++,wp*=w) {
-        c[i] = b[i] + wp*a[i];
-        c[i+n/2]=b[i]-wp*a[i];
+        f[i] = b[i] + wp*a[i];
+        f[i+n/2]=b[i]-wp*a[i];
     }
-    return c;
 }
 
-poly idft(poly f, cpx w) {
+void idft(poly &f, cpx w) {
     int n=f.size();
-    f=dft(f,w);
-    reverse(f.begin()+1,f.end());
+    dft(f,w), reverse(f.begin()+1,f.end());
     transform(f.begin(),f.end(),f.begin(),[&n](auto a){ return a/cpx(n,0); });
-    return f;
 }
 
 poly operator*(poly& a, poly& b) {
@@ -34,9 +31,10 @@ poly operator*(poly& a, poly& b) {
     poly c(n);
 
     cpx w(cos(2*M_PI/n), sin(2*M_PI/n));
-    poly ap=dft(a,w), bp=dft(b,w);
-    for(int i=0;i<n;i++) c[i]=ap[i]*bp[i];
-    return idft(c,w);
+    dft(a,w), dft(b,w);
+    for(int i=0;i<n;i++) c[i]=a[i]*b[i];
+    idft(c,w);
+    return c;
 }
 
 int main()
