abb5cda889451cdafdc3403699d25904e8d65c9b
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-05-09 21:50:41 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, D[1010]={0,1,0,1,1};
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    for(int i=5;i<=N;i++) D[i]=D[i-1] && D[i-3] && D[i-4] ? 0 : 1;
+    cout << (D[N] ? "SK" : "CY");
+    return 0;
+}
