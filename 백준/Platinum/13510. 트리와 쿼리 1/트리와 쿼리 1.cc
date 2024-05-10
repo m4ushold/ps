0a04b6cdf18b866cbaeff1dacbb6bcc96aedf46f
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-05-10 13:05:29 +0900

    [Platinum I] Title: 트리와 쿼리 1, Time: 104 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Platinum/13510. 트리와 쿼리 1/트리와 쿼리 1.cc
+++ 백준/Platinum/13510. 트리와 쿼리 1/트리와 쿼리 1.cc
@@ -2,59 +2,55 @@
 using namespace std;
 using ll=long long;
 
-const int SZ=101010;
-int N, Q, A[SZ], Top[SZ], Par[SZ], Dep[SZ], Sz[SZ], In[SZ];
-vector<int> Inp[SZ], G[SZ];
+constexpr int SZ=1<<17;
+int N, Q, TOP[SZ], P[SZ], D[SZ], S[SZ], IN[SZ];
+vector<int> G[SZ];
+
+inline ll merge(ll a, ll b) { return max(a,b); }
 
 struct SegTree {
-    ll T[1<<18], sz=1<<17;
-    void build() { for(int i=sz-1;i>=1;i--) T[i]=max(T[i*2],T[i*2+1]); }
-    void update(int v, int x) {
-        v|=sz, T[v]=x;
-        while(v>>=1) T[v]=max(T[v*2],T[v*2+1]);
+    ll T[SZ<<1];
+    void build() { for(int i=SZ-1;i>=1;i--) T[i]=merge(T[i*2],T[i*2+1]); }
+    void upd(int i, ll x) {
+        i|=SZ, T[i]=x;
+        while(i>>=1) T[i]=merge(T[i*2],T[i*2+1]);
     }
-    ll query(int l, int r) {
+    ll qry(int l, int r) {
         ll res=0;
-        for(l|=sz,r|=sz ; l<=r ; l/=2,r/=2) {
-            if(l&1) res=max(res,T[l++]);
-            if(~r&1) res=max(res,T[r--]);
+        for(l|=SZ,r|=SZ ; l<=r ; l/=2,r/=2) {
+            if(l&1) res=merge(res,T[l++]);
+            if(~r&1) res=merge(res,T[r--]);
         }
         return res;
     }
 } seg;
 
-void Connect(int u, int v){ Inp[u].push_back(v); Inp[v].push_back(u); }
-
-void DFS0(int v, int b=-1){
-    for(auto i : Inp[v]) if(i != b)
-        Dep[i] = Dep[v] + 1, Par[i] = v, G[v].push_back(i), DFS0(i, v);
-}
-
-void DFS1(int v){
-    Sz[v] = 1;
-    for(auto &i : G[v]){
-        DFS1(i); Sz[v] += Sz[i];
-        if(Sz[i] > Sz[G[v][0]]) swap(i, G[v][0]);
+struct HLD {
+    void f(int v=1, int p=-1) { 
+        S[v]=1;
+        for(auto &i:G[v]) if(i!=p) {
+            D[i]=D[v]+1, P[i]=v, f(i,v), S[v]+=S[i];
+            if(S[G[v][0]] < S[i]) swap(G[v][0],i);
+        } 
     }
-}
-
-void DFS2(int v){
-    static int pv = 0; In[v] = ++pv;
-    for(auto i : G[v]) Top[i] = i == G[v][0] ? Top[v] : i, DFS2(i);
-}
-
-void VertexUpdate(int x, int v) { seg.update(In[x], v); }
-
-ll PathQuery(int u, int v){
-    ll res = 0;
-    for(; Top[u] != Top[v]; u=Par[Top[u]]){
-        if(Dep[Top[u]] < Dep[Top[v]]) swap(u, v);
-        res = max(res,seg.query(In[Top[u]], In[u]));
+    void g(int v=1, int p=-1) {
+        static int pv = 0; IN[v] = ++pv;
+        for(auto i : G[v]) if(i!=p) TOP[i] = i == G[v][0] ? TOP[v] : i, g(i,v);
     }
-    if(In[u] > In[v]) swap(u, v);
-    res = max(res,seg.query(In[u]+1, In[v])); // 정점 쿼리는 In[u], 간선 쿼리는 In[u]+1
-    return res;
-}
+    void build() { f(), g(TOP[1]=1); }
+
+    void upd(int u, int x) { seg.upd(IN[u], x); }
+    ll qry(int u, int v){
+        ll res = 0;
+        for(; TOP[u] != TOP[v]; u=P[TOP[u]]){
+            if(D[TOP[u]] < D[TOP[v]]) swap(u, v);
+            res = merge(res,seg.qry(IN[TOP[u]], IN[u]));
+        }
+        if(IN[u] > IN[v]) swap(u, v);
+        res = merge(res,seg.qry(IN[u]+1, IN[v]));
+        return res;
+    }
+} tree;
 
 int main()
 {
@@ -63,21 +59,21 @@ int main()
     vector<tuple<int,int,int>> E(N);
     for(int i=1;i<N;i++) {
         auto &[s,e,w]=E[i]; cin >> s >> e >> w;
-        Inp[s].push_back(e), Inp[e].push_back(s);
+        G[s].push_back(e), G[e].push_back(s);
     }
-    DFS0(1); DFS1(1); DFS2(Top[1]=1);
+    tree.build();
 
     for(auto &[s,e,w]:E) {
-        if(Dep[s]>Dep[e]) swap(s,e);
-        seg.T[seg.sz|In[e]]=w;
+        if(D[s]>D[e]) swap(s,e);
+        seg.T[SZ|IN[e]]=w;
     }
     seg.build();
 
     cin >> Q;
     for(int i=0,a,b,c;i<Q;i++) {
         cin >> a >> b >> c;
-        if(a&1) VertexUpdate(get<1>(E[b]),c);
-        else cout << PathQuery(b,c) << '\n';
+        if(a&1) tree.upd(get<1>(E[b]),c);
+        else cout << tree.qry(b,c) << '\n';
     }
     return 0;
 }
