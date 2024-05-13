7273789c0783ccc92207d87ed216f75b5b90edef
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-05-13 16:36:41 +0900

    [Bronze III] Title: Error Detection, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5220. Error Detection/Error Detection.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        int n,a; cin >> n >> a;
+        cout << ((__popcount(n)&1)==a ? "Valid\n" : "Corrupt\n");
+    }
+    return 0;
+}
