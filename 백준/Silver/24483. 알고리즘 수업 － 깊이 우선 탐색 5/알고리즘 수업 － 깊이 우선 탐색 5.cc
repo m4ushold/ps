8474994699f70a4adf57f37c3ec2228fec9847a5
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-05-21 18:36:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 80 ms, Memory: 11348 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int N, M, R, D[101010], VI[101010];
+vector<int> G[101010];
+ll A;
+
+void dfs(int v) {
+    static ll ord=0;
+    if(VI[v]) return;
+    VI[v]=1, A+=++ord*D[v];
+    for(int i:G[v]) if(!VI[i]) D[i]=D[v]+1, dfs(i);
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> M >> R;
+    for(int i=0,s,e;i<M;i++) cin >> s >> e, G[s].push_back(e), G[e].push_back(s);
+    for(int i=1;i<=N;i++) sort(G[i].begin(),G[i].end());
+    memset(D,-1,sizeof D), D[R]=0, dfs(R);
+    cout << A;
+    return 0;
+}
