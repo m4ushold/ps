46cba9edbdd543fd0a1de4e24e7544301a555362
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-08-02 16:08:56 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    for(int i=0;i<n;i++) cout << string(n*5,'@') << "\n";
+    for(int i=0;i<n;i++) cout << string(n,'@') << "\n";
+    for(int i=0;i<n;i++) cout << string(n*5,'@') << "\n";
+    for(int i=0;i<n;i++) cout << string(n,'@') << "\n";
+    for(int i=0;i<n;i++) cout << string(n*5,'@') << "\n";
+    return 0;
+}
