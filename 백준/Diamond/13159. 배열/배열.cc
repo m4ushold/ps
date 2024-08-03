ffc3386ca8a075bd7d5c1b0c34710f72d6f67a33
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-08-03 16:48:34 +0900

    [Diamond II] Title: 배열, Time: 312 ms, Memory: 25372 KB -BaekjoonHub

--- 백준/Diamond/13159. 배열/배열.cc
+++ 백준/Diamond/13159. 배열/배열.cc
@@ -2,106 +2,144 @@
 using namespace std;
 using ll=long long;
 
-template <typename T> struct SplayTree {
+template<typename T>
+struct SplayTree {
     struct Node {
-        int l, r, p, cnt, inv;
-        ll mn, mx, sum;
+        Node *l, *r, *p;
+        int cnt, mn, mx, h;
+        ll sum;
         T val;
-        Node() : l(-1), r(-1), p(-1), cnt(0), inv(0), mn(0), mx(0), sum(0) {}
-        // Node(T val) : val(val), l(-1), r(-1), p(-1), cnt(0), inv(0) {}
-    };
+        bool inv;
+        Node(T val) : l(0), r(0), p(0), val(val), cnt(0), inv(0),mn(0),mx(0),sum(0),h(1) {}
+        ~Node(){
+            if(l) delete l;
+            if(r) delete r;
+        }
+    } *tree;
+    Node *vec[303030];
+    int n, wait, cnt;
 
-    int n, sz, root;
-    vector<Node> t;
-
-    SplayTree(vector<T> v) : n(v.size()), t(n+2), sz(n+2), root(0) {
-        t[0].r=1, t[0].cnt=n+1, t[n+1].p=n, t[n+1].cnt=1;
-        for(int i=1;i<=n;i++) t[i].mn=t[i].mx=t[i].sum=t[i].val=v[i-1], t[i].p=i-1, t[i].r=i+1, t[i].cnt=n-i+1;
-        for(int i=n+1;i>=0;i--) upd(i);
+    SplayTree(vector<T> v) : n(v.size()), wait(sqrt(n)), cnt(0) {
+        vec[0]=new Node(0), vec[n+1]=new Node(0);
+        for(int i=1;i<=n;i++) vec[i]=new Node(v[i-1]);
+        for(int i=1;i<=n;i++) vec[i]->p=vec[i-1], vec[i]->r=vec[i+1];
+        vec[0]->r=vec[1], vec[n+1]->p=vec[n];
+        for(int i=n+1;i>=0;i--) update(vec[i]);
+        tree=vec[0];
     }
     
-    void prop(int x) {
-        if(!t[x].inv) return;
-        swap(t[x].l, t[x].r);
-        t[x].inv=0;
-        if(t[x].l>=0) t[t[x].l].inv=!t[t[x].l].inv;
-        if(t[x].r>=0) t[t[x].r].inv=!t[t[x].r].inv;
+    void prop(Node *x) {
+        if (!x->inv) return;
+        swap(x->l, x->r);
+        x->inv=0;
+        if(x->l) x->l->inv^=1;
+        if(x->r) x->r->inv^=1;
     }
-    void upd(int x) {
-        t[x].cnt=1, t[x].mn=t[x].mx=t[x].sum=t[x].val;
-        if(t[x].l>=0) t[x].cnt += t[t[x].l].cnt, t[x].mn=min(t[x].mn, t[t[x].l].mn), t[x].mx=max(t[x].mx, t[t[x].l].mx), t[x].sum+=t[t[x].l].sum;
-        if(t[x].r>=0) t[x].cnt += t[t[x].r].cnt, t[x].mn=min(t[x].mn, t[t[x].r].mn), t[x].mx=max(t[x].mx, t[t[x].r].mx), t[x].sum+=t[t[x].r].sum;
+    
+    void update(Node *x) {
+        if(!x) return;
+        x->cnt = 1;
+        x->mn=x->mx=x->sum=x->val;
+        x->h = 0;
+        if(x->l) x->cnt+=x->l->cnt, x->mn=min(x->mn,x->l->mn), x->mx=max(x->mx, x->l->mx), x->sum+=x->l->sum, x->h=max(x->h, x->l->h);
+        if(x->r) x->cnt+=x->r->cnt, x->mn=min(x->mn,x->r->mn), x->mx=max(x->mx, x->r->mx), x->sum+=x->r->sum, x->h=max(x->h, x->r->h);
+        x->h+=1;
     }
-    void rotate(int x) {
-        int p=t[x].p, b;
-        prop(p), prop(x);
-        if(p==-1) return;
-        if(x==t[p].l) t[p].l=b=t[x].r, t[x].r=p;
-        else t[p].r=b=t[x].l, t[x].l=p;
-        t[x].p=t[p].p, t[p].p=x;
-        if(b>=0) t[b].p=p;
 
-        (t[x].p>=0 ? p==t[t[x].p].l ? t[t[x].p].l : t[t[x].p].r : root) = x;
-        upd(p), upd(x);
+    void rotate(Node *x) {
+        if(!x) return;
+        Node *p=x->p, *b=0;
+        prop(p), prop(x);
+        if(!p) return;
+        if(x==p->l) p->l=b=x->r, x->r=p;
+        else p->r=b=x->l, x->l=p;
+        x->p=p->p, p->p=x;
+        if(b) b->p=p;
+
+        (x->p ? p == x->p->l ? x->p->l : x->p->r : tree) = x;
+        update(p), update(x);
     }
-    void splay(int x) {
-        while(t[x].p>=0) {
-            int p=t[x].p, g=t[p].p;
-            if(g>=0) rotate((x==t[p].l) == (p==t[g].l) ? p : x);
+    void splay_max() {
+        Node *x = tree;
+        while(x->l || x->r) {
+            if(x->l && x->l->h+1 == x->h) x=x->l;
+            else x=x->r;
+        }
+        internal_splay(x);
+    }
+    
+    void splay(Node *x) {
+        if(++cnt == wait) {
+            splay_max();
+            cnt=0;
+        }
+        internal_splay(x);
+    }
+
+    void internal_splay(Node *x) {
+        if(!x) return;
+        while(x->p) {
+            Node *p=x->p, *g=p->p;
+            if(g) rotate((x==p->l) == (p==g->l) ? p : x);
             rotate(x);
         }
         prop(x);
     }
 
     void kth(int k) {
-        int x=root; prop(x);
+        Node *x=tree;
+        if(!x || k>n) return;
+        prop(x);
         while(1) {
-            while(t[x].l>=0 && t[t[x].l].cnt > k) x=t[x].l, prop(x);
-            if(t[x].l>=0) k-=t[t[x].l].cnt;
+            while(x->l && x->l->cnt > k) x=x->l, prop(x);
+            if(x->l) k-=x->l->cnt;
             if(!k--) break;
-            x=t[x].r, prop(x);
+            x=x->r;
+            prop(x);
         }
         splay(x);
     }
 
-    int gather(int l, int r) {
+    Node* gather(int l, int r) {
         kth(l-1);
-        int x=root;
-        root=t[x].r, t[root].p=-1;
+        Node *x = tree;
+        tree=x->r, tree->p=0;
         kth(r-l+1);
-        t[x].r=root, t[root].p=x, root=x;
-        return t[t[root].r].l;
+        x->r=tree, tree->p=x;
+        tree=x;
+        return tree->r->l;
     }
 
     void rev(int l, int r) {
-        if(l>r) return;
-        int x=gather(l,r);
-        t[x].inv=!t[x].inv;
+        if(l>=r) return;
+        Node *x=gather(l,r);
+        if(!x) return;
+        x->inv^=1;
     }
-
-    int mod(int x, int m) { return (x%m+m)%m; }
-    void shift(int l, int r, int x) {
-        x=mod(x,r-l+1), rev(l,r), rev(l,l+x-1), rev(l+x,r);
-    }
-
     void qry(int l, int r) {
-        int x=gather(l,r);
-        cout << t[x].mn << ' ' << t[x].mx << ' ' << t[x].sum << '\n';
+        Node *x=gather(l,r);
+        cout << x->mn << ' ' << x->mx << ' ' << x->sum << '\n';
+    }
+    
+    int mod(int k, int m) { return (k%m+m)%m; }
+    void shift(int l, int r, int k) {
+        k=mod(k,r-l+1), rev(l,r), rev(l,l+k-1), rev(l+k,r);
     }
 
-    void dfs(int x) {
+    void f(Node *x) {
         prop(x);
-        if(t[x].l>=0) dfs(t[x].l);
-        if(t[x].val) cout << t[x].val << ' ';
-        if(t[x].r>=0) dfs(t[x].r);
+        if(x->l) f(x->l);
+        if(x->val) cout << x->val << ' ';
+        if(x->r) f(x->r);
     }
-
-    void print() {
-        dfs(root);
-        cout << '\n';
+    void p() {
+        f(tree), cout << '\n';
     }
 };
 
+int mod(int x, int m) {
+    return (x%m+m)%m;
+}
 
 int main()
 {
@@ -114,10 +152,10 @@ int main()
     for(int i=0,a,l,r,x,j;i<q;i++) {
         cin >> a >> l;
         if(a==1) cin >> r, tree.qry(l,r), tree.rev(l,r);
-        else if(a==2) cin >> r >> x, tree.qry(l,r), tree.shift(l,r,x);
-        else if(a==3) tree.kth(l), cout << tree.t[tree.root].val << '\n';
-        else tree.splay(l), cout << tree.t[tree.t[tree.root].l].cnt << '\n';
+        else if(a==2) cin >> r >> x, x=mod(x,r-l+1), tree.qry(l,r), tree.rev(l,r), tree.rev(l,l+x-1), tree.rev(l+x,r);
+        else if(a==3) tree.kth(l), cout << tree.tree->val << '\n';
+        else tree.splay(tree.vec[l]), cout << tree.tree->l->cnt << '\n';
     }
-    tree.print();
+    tree.p();
     return 0;
 }
