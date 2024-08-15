596f15a746c2e914b5aa5acf45bc535a2fce3909
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-08-15 17:31:36 +0900

    [Bronze III] Title: Hot Dogs, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5666. Hot Dogs/Hot Dogs.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int h,p;
+    while(cin >> h >> p) cout << fixed << setprecision(2) << 1.*h/p << '\n';
+    return 0;
+}
