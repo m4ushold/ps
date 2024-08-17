ae9dcf450c6d6e9bf1302b51ca48a8380a72fce6
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-08-17 17:03:37 +0900

    [Bronze III] Title: 접시 안의 원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll t; cin >> t;
+    cout << (int)round(t*t/4.0);
+    return 0;
+}
