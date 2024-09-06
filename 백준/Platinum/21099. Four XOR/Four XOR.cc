f082a84fa3a1b03227c35bf96fe17dbc01a0d83f
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-09-06 19:19:01 +0900

    [Platinum IV] Title: Four XOR, Time: 8 ms, Memory: 2396 KB -BaekjoonHub

--- 백준/Platinum/21099. Four XOR/Four XOR.cc
+++ 백준/Platinum/21099. Four XOR/Four XOR.cc
@@ -6,7 +6,7 @@ int main() {
     int n; cin >> n;
     vector<int> v(n);
     for(int &i:v) cin >> i;
-    if(n>100) return cout << "Yes", 0;
+    if(n>256) return cout << "Yes", 0;
 
     for(int i=0;i<n;i++) for(int j=i+1;j<n;j++) for(int k=j+1;k<n;k++) for(int w=k+1;w<n;w++) {
         if((v[i]^v[j]^v[k]^v[w]) == 0) return cout << "Yes", 0;
