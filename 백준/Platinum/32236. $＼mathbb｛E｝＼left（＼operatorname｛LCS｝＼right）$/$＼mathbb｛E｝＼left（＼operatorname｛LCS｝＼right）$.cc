11571cb670e80333d9cb4ca2503a5eabc9a9ab90
m4ushold <160091510+m4ushold@users.noreply.github.com>
2024-09-10 14:09:24 +0900

    [Platinum II] Title: $\mathbb{E}\left(\operatorname{LCS}\right)$, Time: 0 ms, Memory: 2020 KB, Score: 1 point -BaekjoonHub

---
+++ 백준/Platinum/32236. $＼mathbb｛E｝＼left（＼operatorname｛LCS｝＼right）$/$＼mathbb｛E｝＼left（＼operatorname｛LCS｝＼right）$.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+const int MOD=1e9+7;
+
+ll pw(ll a, ll n) {
+    ll res=1;
+    while(n) {
+        if(n&1) res=res*a%MOD;
+        a=a*a%MOD, n/=2;
+    }
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll n,m; cin >> n >> m;
+    ll dap=0;
+    for(int i=1;i<=n;i++) {
+        dap+=pw(pw(i,m),MOD-2);
+        cout << dap << ' ';
+    }
+    return 0;
+}
