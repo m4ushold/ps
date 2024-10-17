de56a6ff4b31df4868c92a2605b3051a03b00cc3
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-10-17 17:15:14 +0900

    [Platinum II] Title: 마지막 자리, Time: 248 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/4862. 마지막 자리/마지막 자리.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+constexpr int SZ = 1010101;
+
+ll pw(__int128_t a, ll n, ll mod) {
+    __int128_t res=1;
+    while(n) {
+        if(n&1) res=res*a%mod;
+        a=a*a%mod, n/=2;
+    }
+    return res;
+}
+
+ll phi(ll n) {
+	ll res=n;
+	for(ll i=2;i*i<=n;i++) {
+		if(n%i==0) {
+			res-=res/i;
+			while(n%i==0) n/=i;
+		}
+	}
+	if(n!=1) res-=res/n;
+	return res;
+}
+
+bool PowOverflow(ll a, ll b, ll c){
+    __int128_t res = 1;
+    bool flag = false;
+    for(; b; b >>= 1, a = a * a){
+        if(a >= c) flag = true, a %= c;
+        if(b & 1){
+        res *= a; if(flag || res >= c) return true;
+        }
+    }
+    return false;
+}
+ll Recursion(int idx, ll mod, const vector<ll> &vec){
+    if(mod == 1) return 1;
+    if(idx + 1 == vec.size()) return vec[idx];
+    ll nxt = Recursion(idx+1, phi(mod), vec);
+    if(PowOverflow(vec[idx], nxt, mod)) return pw(vec[idx], nxt, mod) + mod;
+    else return pw(vec[idx], nxt, mod);
+}
+ll PowerTower(const vector<ll> &vec, ll mod){ // vec[0]^(vec[1]^(vec[2]^(...))), suppose vec[i] >= 2
+    if(vec.size() == 1) return vec[0] % mod;
+    else return pw(vec[0], Recursion(1, phi(mod), vec), mod);
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll b,n,i;
+    while(cin >> b && b) {
+        cin >> i >> n;
+        vector<ll> v(i, b);
+        ll ans, b=1;
+        for(int i=1;i<=n;i++) b*=10;
+        if(b==1) ans=1;
+        else ans=PowerTower(v, b);
+        cout << setw(n) << setfill('0') << ans << '\n';
+    }
+    return 0;
+}
