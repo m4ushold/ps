213ac53caf44e50e0f6161de69ea7ede87ad8017
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-10-17 18:12:59 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    if(n==2) cout << 3;
+    else cout << n;
+    return 0;
+}
