e92fa6a014a4311822d019d333fcfc6d73363e45
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-10-21 13:03:07 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    if(n==1) cout << 2;
+    else cout << 1;
+    return 0;
+}
