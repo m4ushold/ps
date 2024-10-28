34b611a81ffa834f51c2d9991791ff4121b6ef07
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-10-28 15:17:25 +0900

    [Bronze III] Title: Walk Like an Egyptian, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4922. Walk Like an Egyptian/Walk Like an Egyptian.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n;
+    while(cin >> n && n) cout << n << " => " << n*n-n+1 << '\n';
+    return 0;
+}
