2712088c7d56fbbae01fb7cc84ab02b8f75a1348
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-11-01 22:05:12 +0900

    [Platinum IV] Title: 단절점, Time: 24 ms, Memory: 4728 KB -BaekjoonHub

--- 백준/Platinum/11266. 단절점/단절점.cc
+++ 백준/Platinum/11266. 단절점/단절점.cc
@@ -6,19 +6,17 @@ const int MAX_V=10101;
 vector<int> G[MAX_V];
 int In[MAX_V], Low[MAX_V], P[MAX_V];
 
-void tarjan(int n){
-    int c=0;
-    function<void(int,int)> dfs = [&](int v, int p){
-        In[v]=Low[v]=++c, P[v]=p;
-        for(auto i:G[v]) if(i!=p) {
-            if(!In[i]) dfs(i,v), Low[v]=min(Low[v], Low[i]); 
-            else Low[v]=min(Low[v], In[i]);
-        }
-    };
-    for(int i=1;i<=n;i++) if(!In[i]) dfs(i, 0);
+void dfs(int v, int p=0) {
+    static int pv=0;
+    In[v]=Low[v]=++pv, P[v]=p;
+    for(auto i:G[v]) if(i!=p) {
+        if(!In[i]) dfs(i,v), Low[v]=min(Low[v], Low[i]);
+        else Low[v]=min(Low[v], In[i]);
+    }
 }
+
 vector<int> cutVertex(int n){
-    tarjan(n);
+    for(int i=1;i<=n;i++) if(!In[i]) dfs(i);
     vector<int> res, isCut(MAX_V);
     function<void(int)> dfs=[&](int v){
         int chd=0;
@@ -33,9 +31,8 @@ vector<int> cutVertex(int n){
     return res;
 }
 
-int main()
-{
-    ios::sync_with_stdio(0); cin.tie(0);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
     int n, m; cin >> n >> m;
     for(int i=0,a,b;i<m;i++) cin >> a >> b, G[a].push_back(b), G[b].push_back(a);
     vector<int> v=cutVertex(n);
