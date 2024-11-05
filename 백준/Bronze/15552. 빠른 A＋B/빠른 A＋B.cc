2abdaaf1eb5a872d1dbaca9d69f441e96d72cd8c
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-11-05 22:10:22 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0); cin.tie(0);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
     int t; cin >> t;
     while(t--) {
         int a,b; cin >> a >> b;
