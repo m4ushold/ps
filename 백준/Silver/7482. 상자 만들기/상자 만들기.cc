29e9eb415771cbc6d8e1fe3592ac983989eb6392
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-12-07 12:29:47 +0900

    [Silver V] Title: 상자 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7482. 상자 만들기/상자 만들기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t; cin >> t;
+    while(t--) {
+        double a; cin >> a;
+        cout << fixed << setprecision(10) << a/6 << '\n';
+    }
+    return 0;
+}
