033ab067156fef9ecefa040311edb53b092ecb3f
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-12-20 09:40:02 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 452 ms, Memory: 247288 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -4,22 +4,19 @@ n = input()
 arr = list(map(int, input().split()))
 dp = []
 ans = 0
-def bs_left(dp, target):
+for i in range(0,len(arr)):
     left = 0
     right = len(dp) - 1
+    target = arr[i]
     while(left <= right):
         mid = (left + right) // 2
         if(dp[mid] < target):
             left = mid + 1
         else:
             right = mid - 1
-    return left
-
-for i in range(0,len(arr)):
-    left = bs_left(dp, arr[i])
     if(left == len(dp)):
-        dp.append(arr[i])
+        dp.append(target)
         ans += 1
     else:
-        dp[left] = arr[i]
+        dp[left] = target
 print(ans)
