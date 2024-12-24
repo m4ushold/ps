ad678e81cb006e30814dbb9b19cac6811ce5d417
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2024-12-24 14:21:23 +0900

    [Platinum V] Title: 비용, Time: 40 ms, Memory: 5948 KB -BaekjoonHub

---
+++ 백준/Platinum/2463. 비용/비용.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+const int MOD=1e9;
+
+ll P[101010], S[101010];
+void init(int n) { for(int i=1;i<=n;i++) P[i]=i,S[i]=1; }
+int Find(int v) { return P[v]==v?v:P[v]=Find(P[v]); }
+bool Union(int u, int v) {return (u=Find(u))!=(v=Find(v)) && (S[u]>S[v]?swap(u,v):void(), P[u]=v, S[v]+=S[u], true); }
+
+int main(void) {
+    cin.tie(0)->sync_with_stdio(0);
+    int n,m; cin >> n >> m;
+    vector<array<ll,3>> v(m);
+    ll dap=0,sum=0,tot=0;
+    for(auto &[w,u,v]:v) cin >> u >> v >> w, tot+=w;
+    sort(v.begin(),v.end(),greater<>());
+    init(n);
+
+    for(auto [w,u,v]:v) {
+        u=Find(u), v=Find(v);
+        if(u^v) {
+            dap+=S[u]*S[v]*tot;
+            dap%=MOD;
+            Union(u,v);
+        }
+        tot-=w;
+    }
+    cout << dap;
+    return 0;
+}
