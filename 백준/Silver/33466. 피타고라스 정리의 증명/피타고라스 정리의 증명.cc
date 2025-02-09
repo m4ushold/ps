bd87b7c665fe956d829c7d659c74ee44e1c57485
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-02-09 19:22:21 +0900

    [Silver I] Title: 피타고라스 정리의 증명, Time: 352 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/33466. 피타고라스 정리의 증명/피타고라스 정리의 증명.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        cout << n+n/2*2 << '\n';
+    }
+    return 0;
+}
