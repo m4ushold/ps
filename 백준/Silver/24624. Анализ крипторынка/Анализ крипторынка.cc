cc97ac686f2418a1f2ad809dc0b2370577b998d4
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-03-25 16:45:20 +0900

    [Silver IV] Title: Анализ крипторынка, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24624. Анализ крипторынка/Анализ крипторынка.cc
+++ 백준/Silver/24624. Анализ крипторынка/Анализ крипторынка.cc
@@ -5,8 +5,6 @@ using ll=long long;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     ll a,b,c; cin >> a >> b >> c;
-    // a^x + b^x = c^x
-    // fermat's last theorem - 1,2만 확인
     if(a + b == c) cout << 1;
     else if(a*a + b*b == c*c) cout << 2;
     else cout << -1;
