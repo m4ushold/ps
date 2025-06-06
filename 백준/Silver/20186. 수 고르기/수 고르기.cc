9495e586e0cba551bf7188709d0126ae5c6dca38
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-06-06 13:45:33 +0900

    [Silver IV] Title: 수 고르기, Time: 0 ms, Memory: 2160 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/20186. 수 고르기/수 고르기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, k; cin >> n >> k;
+    vector<int> v(n);
+    for(int &i:v) cin >> i;
+    sort(v.begin(),v.end(),greater<>());
+    int ans=0, cnt=0;
+    for(int i:v) {
+        ans += max(i-cnt, 0);
+        cnt++;
+        k--;
+        if(k == 0) break;
+    }
+    cout << ans;
+    return 0;
+}
