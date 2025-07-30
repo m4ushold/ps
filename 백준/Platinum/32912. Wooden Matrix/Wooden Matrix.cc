9d5a5bfe3a23dc2d43d03734264540abaeb1239b
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-07-30 22:29:03 +0900

    [Platinum IV] Title: Wooden Matrix, Time: 192 ms, Memory: 28596 KB -BaekjoonHub

--- 백준/Platinum/32912. Wooden Matrix/Wooden Matrix.cc
+++ 백준/Platinum/32912. Wooden Matrix/Wooden Matrix.cc
@@ -2,25 +2,13 @@
 using namespace std;
 using ll=long long;
 
-ll N, D[1010], P[22][1010], A[1010][1010], W[1010];
+ll N, D[1010], A[1010][1010];
 vector<pair<int,int>> G[1010];
-void dfs(int v, int b=-1) { for(auto [i,w]:G[v]) if(i!=b) D[i]=D[v]+1, W[i]=W[v]+w, P[0][i]=v, dfs(i,v); }
-
-int lca(int u, int v) {
-    if(D[u]<D[v]) swap(u,v);
-    int diff=D[u]-D[v];
-    for(int i=0;diff;i++,diff>>=1) if(diff&1) u=P[i][u];
-    if(u==v) return u;
-    for(int i=21;i>=0;i--) if(P[i][u]!=P[i][v]) u=P[i][u], v=P[i][v];
-    return P[0][u];
-}
-int dst(int u, int v) {
-    int l=lca(u,v);
-    return W[u]+W[v]-W[l]-W[l];
-}
-void build() {
-    dfs(1);
-    for(int i=1;i<22;i++) for(int j=1;j<=N;j++) P[i][j]=P[i-1][P[i-1][j]];
+void dfs(int v, int p=-1) {
+    for(auto [i,w]:G[v]) if(i!=p) {
+        D[i]=D[v]+w;
+        dfs(i,v);
+    }
 }
 
 int PP[1010], S[1010];
@@ -41,10 +29,13 @@ int main() {
     for(auto [w,u,v]:ed) {
         if(Union(u,v)) G[u].push_back({v,w}), G[v].push_back({u,w});
     }
-    build();
 
-    for(int i=1;i<=N;i++) for(int j=i+1;j<=N;j++) {
-        if(A[i][j] != dst(i,j)) return cout << "No", 0;
+    for(int i=1;i<=N;i++) {
+        memset(D, 0, sizeof(D));
+        dfs(i);
+        for(int j=1;j<=N;j++) if(D[j] != A[i][j]) {
+            return cout << "No", 0;
+        }
     }
     cout << "Yes";
     return 0;
