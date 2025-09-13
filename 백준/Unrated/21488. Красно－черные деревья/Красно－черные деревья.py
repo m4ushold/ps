bd90d0ce36989bd9f0386de8e4eafe0cbe0fb1b9
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-09-13 16:33:37 +0900

    [Unrated] Title: Красно-черные деревья, Time: 252 ms, Memory: 445396 KB -BaekjoonHub

--- 백준/Unrated/21488. Красно－черные деревья/Красно－черные деревья.py
+++ 백준/Unrated/21488. Красно－черные деревья/Красно－черные деревья.py
@@ -1,62 +1,59 @@
 import sys
+sys.setrecursionlimit(200000)
 
-def input():
-    return sys.stdin.readline().rstrip()
+N = 0
+L = []
+R = []
+IN = []
+D = []
 
-MAX_N = 1011
+def dfs(v):
+    if L[v]:
+        dfs(L[v])
+    if R[v]:
+        dfs(R[v])
 
-DP = [[[0] * 2 for _ in range(MAX_N)] for _ in range(MAX_N)]
+    for i in range(N+1):
+        D[v][i][0] += D[L[v]][i][1] * D[R[v]][i][1]
 
-child = [[0, 0] for _ in range(MAX_N)]
+        if i > 0:
+            D[v][i][1] += (
+                D[L[v]][i-1][0] * D[R[v]][i-1][0] +
+                D[L[v]][i-1][0] * D[R[v]][i-1][1] +
+                D[L[v]][i-1][1] * D[R[v]][i-1][0] +
+                D[L[v]][i-1][1] * D[R[v]][i-1][1]
+            )
 
-check = [False] * MAX_N
+def sol():
+    global N, L, R, IN, D
+    N = int(sys.stdin.readline())
+    L = [0] * (N+1)
+    R = [0] * (N+1)
+    IN = [0] * (N+1)
 
-def dfs(cur):
-    if child[cur][0] == 0 and child[cur][1] == 0:
-        DP[cur][1][0] = 1
-        DP[cur][0][1] = 1
-        return
+    for i in range(1, N+1):
+        a, b = map(int, sys.stdin.readline().split())
+        L[i], R[i] = a, b
+        if a:
+            IN[a] += 1
+        if b:
+            IN[b] += 1
 
-    left = child[cur][0]
-    right = child[cur][1]
-
-    if left != 0:
-        dfs(left)
-    if right != 0:
-        dfs(right)
-    for i in range(1, MAX_N):
-        DP[cur][i][0] += DP[left][i - 1][0] * DP[right][i - 1][1]
-        DP[cur][i][0] += DP[left][i - 1][0] * DP[right][i - 1][0]
-        DP[cur][i][0] += DP[left][i - 1][1] * DP[right][i - 1][1]
-        DP[cur][i][0] += DP[left][i - 1][1] * DP[right][i - 1][0]
-
-    for i in range(MAX_N):
-        DP[cur][i][1] += DP[left][i][0] * DP[right][i][0]
-
-
-N = int(input())
-
-for i in range(1, N + 1):
-    l, r = map(int, input().split())
-    child[i][0] = l
-    child[i][1] = r
-        
-    if l != 0:
-        check[l] = True
-    if r != 0:
-        check[r] = True
-    DP[0][0][0] = 1
     root = 0
-for i in range(1, N + 1):
-    if not check[i]:
-        root = i
-        break
+    for i in range(1, N+1):
+        if IN[i] == 0:
+            root = i
+            break
 
-dfs(root)
-ans = 0
-for i in range(N + 1):
-    ans += DP[root][i][0]
-    ans += DP[root][i][1]
+    D = [[[0,0] for _ in range(N+1)] for __ in range(N+1)]
+    D[0][0][1] = 1
 
-print(ans)
+    dfs(root)
 
+    ans = 0
+    for i in range(N+1):
+        ans += D[root][i][0] + D[root][i][1]
+    print(ans)
+
+if __name__ == "__main__":
+    sol()
