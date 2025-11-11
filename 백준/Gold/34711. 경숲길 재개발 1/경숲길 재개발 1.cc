f3b74df426c15d86d6333a2f88dc21ed951a4022
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-11-11 21:40:30 +0900

    [Gold V] Title: 경숲길 재개발 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34711. 경숲길 재개발 1/경숲길 재개발 1.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define prs(v) sort(all(v)), v.erase(unique(all(v)),v.end())
+#define endl '\n'
+using namespace std;
+using ll=long long;
+using pll=pair<ll,ll>;
+
+void sol(int tc) {
+    ll n; cin >> n;
+    ll ans=0;
+    while(n) {
+        ans += n;
+        n/=2;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t=1;
+    // cin >> t;
+    for(int i=1;i<=t;i++) sol(i);
+    return 0;
+}
