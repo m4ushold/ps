30ba63de49c9958dfff85e395ac3acf8e58271a0
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2025-12-10 12:24:40 +0900

    [Platinum I] Title: 이동, Time: 56 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Platinum/1067. 이동/이동.cc
+++ 백준/Platinum/1067. 이동/이동.cc
@@ -1,56 +1,79 @@
 #include <bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define prs(v) sort(all(v)), v.erase(unique(all(v)),v.end())
+#define endl '\n'
 using namespace std;
 using ll=long long;
+using pll=pair<ll,ll>;
 using cpx=complex<double>;
 using poly=vector<cpx>;
 
-poly dft(const poly &f, cpx w) {
-    int n=f.size();
-    if(n==1) return f;
-
-    poly a,b,c(n);
-    for(int i=0;i<n;i++) i&1 ? a.push_back(f[i]) : b.push_back(f[i]);
-    a=dft(a, w*w), b=dft(b,w*w);
-
-    cpx wp(1,0);
-    for(int i=0;i<n/2;i++,wp*=w) {
-        c[i] = b[i] + wp*a[i];
-        c[i+n/2]=b[i]-wp*a[i];
+void fft(poly &f, bool inv) {
+    int n = f.size();
+    for(int i=1, j=0; i<n; i++) {
+        int bit = n/2;
+        while(j >= bit) j -= bit, bit /= 2;
+        j += bit;
+        if(i < j) swap(f[i], f[j]);
     }
-    return c;
+
+    for(int k=1; k<n; k*=2) {
+        cpx w(cos(inv ? M_PI/k : -M_PI/k), sin(inv ? M_PI/k : -M_PI/k));
+        for(int i=0; i<n; i+=k*2) {
+            cpx wp(1, 0);
+            for(int j=0; j<k; j++,wp*=w) {
+                cpx a=f[i+j], b=f[i+j+k]*wp;
+                f[i+j]=a+b, f[i+j+k]=a-b;
+            }
+        }
+    }
+
+    if(inv) for(int i=0; i<n; i++) f[i] /= n;
 }
 
-poly idft(poly f, cpx w) {
-    int n=f.size();
-    f=dft(f,w);
-    reverse(f.begin()+1,f.end());
-    transform(f.begin(),f.end(),f.begin(),[&n](auto a){ return a/cpx(n,0); });
-    return f;
-}
-
-poly operator*(poly& a, poly& b) {
-    int n=1<<(__lg(a.size()+b.size())+1);
+poly operator*(poly a, poly b) {
+    int n=1;
+    while(n<a.size()+b.size()) n<<=1;
     a.resize(n), b.resize(n);
-    poly c(n);
-
-    cpx w(cos(2*M_PI/n), sin(2*M_PI/n));
-    poly ap=dft(a,w), bp=dft(b,w);
-    for(int i=0;i<n;i++) c[i]=ap[i]*bp[i];
-    return idft(c,w);
+    fft(a,0), fft(b,0);
+    for(int i=0;i<n;i++) a[i]*=b[i];
+    fft(a,1);
+    return a;
 }
 
-int main()
-{
-    ios::sync_with_stdio(0); cin.tie(0);
-    ll n,ans=0; cin >> n;
-    poly a(n*2),b(n*2),c;
+void sol(int tc) {
+    int n;
+poly a, b;
 
-    for(int i=0,c;i<n;i++) cin >> a[i];
-    for(int i=n;i<2*n;i++) a[i]=a[i-n];
-    for(int i=n*2-1;i>=n;i--) cin >> b[i];
+  cin.tie(0); cout.tie(0);
 
-    c=a*b;
-    for(auto i:c) ans=max(ans,(ll)round(i.real()));
-    cout << ans;
+  cin >> n;
+  // a = vector<int>(n);
+  // b = vector<int>(n);
+  a = b = poly(n);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    a[i] = cpx(x, 0);
+  }
+  for (int i = 0; i < n; i++) a.push_back(a[i]);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    b[i] = cpx(x, 0);
+  }
+  reverse(b.begin(), b.end());
+  auto x = a*b;
+  int r = 0;
+  for (int i = n; i < 2*n; i++) r = max(r, (int)round(x[i].real()));
+  cout << r;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t=1;
+    // cin >> t;
+    for(int i=1;i<=t;i++) sol(i);
     return 0;
 }
