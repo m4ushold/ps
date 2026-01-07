9c8d4d5d27bce294d52a74918adcebb88a6759df
Minsung Jin <160091510+m4ushold@users.noreply.github.com>
2026-01-07 23:24:27 +0900

    [Gold I] Title: 구간 합 구하기, Time: 188 ms, Memory: 10212 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -2,32 +2,27 @@
 using namespace std;
 using ll=long long;
 
-struct Fenwick {
-    const static int sz=1<<20;
+const int sz=1<<20;
+struct {
     ll T[sz];
+    void add(int i, ll x) {for(;i<sz;i+=i&-i) T[i]+=x;}
     ll sum(int i) {
         ll res=0;
-        while(i) res+=T[i], i-=i&(-i);
+        for(;i;i-=i&-i) res+=T[i];
         return res;
     }
     ll sum(int l, int r) {
-        return sum(r)-(l?sum(l-1):0);
-    }
-    void add(int i, ll x) {
-        while(i<sz) T[i]+=x, i+=i&(-i);
-    }
-    void add(int l, int r, ll x) {
-        add(l, x), add(r+1, x);
+        return sum(r)-sum(l-1);
     }
 } fw;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n,m,k; cin >> n >> m >> k;
-    for(ll i=1,a;i<=n;i++) cin >> a, fw.add(i, a);
-    for(ll i=0,a,b,c;i<m+k;i++) {
+    for(ll i=1,x;i<=n;i++) cin >> x, fw.add(i,x);
+    for(ll i=1,a,b,c;i<=m+k;i++) {
         cin >> a >> b >> c;
-        if(a==1) fw.add(b, c-fw.sum(b,b));
+        if(a == 1) fw.add(b, c-fw.sum(b,b));
         else cout << fw.sum(b,c) << '\n';
     }
     return 0;
